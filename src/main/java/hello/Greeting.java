80ae5d4ab0486b3d1b058d8fc01e6a04364c423c
Gustavo Pacheco <ryctabo@gmail.com>
2017-11-26 13:23:29 -0500

    Update DTO

--- src/main/java/hello/Greeting.java
+++ src/main/java/hello/Greeting.java
@@ -8,8 +8,7 @@ public class Greeting {
 
     private String content;
 
-    public Greeting() {
-    }
+    public Greeting() { }
 
     public Greeting(String content) {
         this.content = content;
